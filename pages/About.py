d0f4c27afad33995a933b5037761693cf87ff6b2
MK <99416359+MK316@users.noreply.github.com>
2025-07-10 14:56:55 +0900

    Create About.py

---
+++ pages/About.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("This page offers interactive apps and resources designed to help English education majors prepare for the Teacher Certification Exam.")
+st.caption("Since July 10, 2025")
