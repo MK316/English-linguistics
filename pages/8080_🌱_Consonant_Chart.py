3b2ab9fa18e45439f41029cb9748c1971da91407
MK <99416359+MK316@users.noreply.github.com>
2025-07-19 16:25:25 +0900

    Update 8080_🌱_Consonant_Chart.py

--- pages/8080_🌱_Consonant_Chart.py
+++ pages/8080_🌱_Consonant_Chart.py
@@ -8,84 +8,11 @@ st.title("🌱 Final IPA Vowel Chart")
 tab1, tab2, tab3 = st.tabs(["English Consonants", "Allophones", "Diacritics"])
 
 with tab1:
-    st.markdown("""
-    <style>
-    table {
-        border-collapse: collapse;
-        margin-top: 1rem;
-        width: 600px;
-    }
-    td {
-        border: none;
-        padding: 0.8em;
-        text-align: center;
-        vertical-align: middle;
-        font-size: 1.3em;
-    }
-    th {
-        border: none;
-        padding: 0.8em;
-        font-size: 1.3em;
-        font-weight: bold;
-    }
-    .orange {
-        color: orange;
-        font-weight: bold;
-    }
-    .rowlabel {
-        text-align: center;
-        vertical-align: middle;
-        font-weight: bold;
-    }
-    .bottomcell {
-        vertical-align: bottom;
-        height: 4em;
-    }
-    </style>
+    st.markdown("Consonant chart")
 
-    <table>
-        <thead>
-            <tr>
-                <th style="text-align:center; vertical-align:middle;"></th>
-                <th style="text-align:center; vertical-align:middle;">Front</th>
-                <th style="text-align:center; vertical-align:middle;">(Central)</th>
-                <th style="text-align:center; vertical-align:middle;">Back</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="rowlabel">High</td>
-                <td>i<br>ɪ</td>
-                <td></td>
-                <td>u<br>ʊ</td>
-            </tr>
-            <tr>
-                <td class="rowlabel">(Mid)</td>
-                <td><span class="orange">e</span><br>ɛ</td>
-                <td>ə<br>ʌ (ɜ)</td>
-                <td><span class="orange">o</span><br>ɔ</td>
-            </tr>
-            <tr>
-                <td class="rowlabel">Low</td>
-                <td>æ</td>
-                <td class="bottomcell"><span class="orange">a</span></td>
-                <td class="bottomcell">ɑ / ɒ</td>
-            </tr>
-        </tbody>
-    </table>
-    """, unsafe_allow_html=True)
+with tab1:
+    st.image("https://raw.githubusercontent.com/MK316/English-linguistics/main/pages/images/Cchart.png", caption="Consonant Chart", use_column_width=True)
 
-    st.markdown("""
-    #### 🚩 Notes: 
-
-    Vowel inventories vary significantly depending on English dialects.
-
-    1. /e/ and /o/ are considered either monophthongs or parts of diphthongs depending on the dialect — e.g., /eɪ/, /oʊ/.
-
-    2. /ɒ/ is a rounded version of /ɑ/.
-
-    3. /ɜ/ is a contextual variant — for example, it appears in r-colored vowels or after r-deletion in some dialects.
-    """)
 
 with tab2:
     st.markdown("""
