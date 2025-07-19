d872a754ac6e65fbca48f34e7bdda082eaba6ac3
MK <99416359+MK316@users.noreply.github.com>
2025-07-19 13:13:57 +0900

    Update 8080_🌱_Vowel_Chart.py

--- pages/8080_🌱_Vowel_Chart.py
+++ pages/8080_🌱_Vowel_Chart.py
@@ -61,6 +61,7 @@ th {
             <td class="rowlabel">Low</td>
             <td>æ</td>
             <td><span class="orange">a</span></td>
+            <td></td>
             <td>ɑ</td>
         </tr>
     </tbody>
