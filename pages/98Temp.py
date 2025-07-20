f06a34f86718c48565a83d89ed62703bb242311d
MK <99416359+MK316@users.noreply.github.com>
2025-07-20 21:43:02 +0900

    Create 98Temp.py

---
+++ pages/98Temp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown("#### More Readings: temporary files")
+st.markdown("---")
+st.markdown("[7](https://drive.google.com/file/d/1w7dJqZbU8DewIGd7ot2bNZ390IKOr_vX/view?usp=drive_link)")
+st.caption("Distinctive Features")
+st.markdown("[8](https://drive.google.com/file/d/1d5hrtPnyuwXQ05Et_A8CkQEzIoWKIQzu/view?usp=drive_link)")
+st.caption("Stress")
+st.markdown("[9](https://drive.google.com/file/d/1WOvvIRIB5SggG_U2PK52YaZHmE5ar2sP/view?usp=drive_link)")
+st.caption("Foot")
+st.markdown("[10](https://drive.google.com/file/d/1vpd-IGQvFCrkQRbwqc2h-5Fkp2U5CyQw/view?usp=drive_link)")
+st.caption("Intonation")
+
