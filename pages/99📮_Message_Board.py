059d6bb92d2308f03963137e22e9058ed646279d
MK <99416359+MK316@users.noreply.github.com>
2025-08-31 23:34:47 +0900

    Update 99📮_Message_Board.py

--- pages/99📮_Message_Board.py
+++ pages/99📮_Message_Board.py
@@ -5,7 +5,7 @@ def main():
     st.caption("💙 Greetings! Feel free to leave any feedback, suggestions, or messages about the application on this page. I'll make sure to look into them as soon as I can! 😍")
     st.write("➡️ Click the '+' sign to write.")
     # Padlet embed URL (you need to replace this with your actual Padlet embed URL)
-    padlet_url = "https://padlet.com/mirankim316/messageboard"
+    padlet_url = "https://padlet.com/mirankim316/F25_engling"
 
     # Create an iframe to embed the Padlet
     padlet_iframe = f"<iframe src='{padlet_url}' width='100%' height='600' frameborder='0' allow='autoplay'></iframe>"
