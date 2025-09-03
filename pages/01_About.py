dc2381d89010f6ec5d9ca9cb792c5be1de278b06
MK <99416359+MK316@users.noreply.github.com>
2025-09-03 17:28:34 +0900

    Update 01_About.py

--- pages/01_About.py
+++ pages/01_About.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 
-tab1, tab2 = st.tabs(["Overview", "Schedule"])
+tab1, tab2 = st.tabs(["🍰 Overview", "🍋 Schedule"])
 
 # --- Tab 1: Overview ---
 with tab1:
@@ -25,12 +25,12 @@ with tab2:
         """
 | Week | Date | Slide   | Reading     | Quiz      |
 |-----:|:-----|:--------|:------------|:----------|
-| 1    | TBD  | Slide 1 | AEP Ch. 1   | —         |
-| 2    | TBD  | Slide 2 | AEP Ch. 2   | Quiz 1    |
-| 3    | TBD  | Slide 3 | AEP Ch. 3   | —         |
-| 4    | TBD  | Slide 4 | AEP Ch. 4   | Quiz 2    |
-| 5    | TBD  | Slide 5 | AEP Ch. 5   | —         |
-| 6    | TBD  | Slide 6 | AEP Ch. 6   | Quiz 3    |
-| 7    | TBD  | Slide 7 | Review Pack | Final Quiz|
+| 1    | Sept. 3  | Slides 1~45 |   | —         |
+| 2    | Sept. 10  | Slides 46~ | AEP Ch. 1&2   | Quiz #1 (Group 1)    |
+| 3    | Sept. 17  | Slides  | AEP Ch. 3   | Quiz #2 (Group 2)        |
+| 4    | Sept. 24  | Slides  | AEP Ch. 4   |  Quiz #3 (Group 3)   |
+| 5    | Oct. 1*  | Slides  | AEP Ch. 6   | Quiz #4 (Group 4)        |
+| 6    | TBA  | Slide 6 | AEP Ch. 7   | ---   |
+| 7    | TBA  | Slide 7 | Review Pack | Quiz #5 (Final Quiz)|
         """
     )
